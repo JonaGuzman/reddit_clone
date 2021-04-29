35ef02c53567c2b1d37706a87bc9a6d7984def45
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2021-04-29 07:11:26 -0400

    added abilty to add/delete votes

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework import generics, permissions, mixins, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from .models import Post, Vote
+from .serializers import PostSerializer, VoteSerializer
 
 class PostList(generics.ListCreateAPIView):
     """ Class based view listing info"""
@@ -9,6 +11,27 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
+
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
+    """ Class based view listing info"""
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+    
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post :)')
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+    
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You never voted for this post!')
